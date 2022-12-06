b38178a06ef7040ed7085a1b1289b0e5547a5f55
ozen0416 <enzo.oudin@ynov.com>
2022-12-06 11:18:37 +0100

    debut du main

--- server.go
+++ server.go
@@ -1,1 +1,26 @@
 package main
+
+import (
+	"hmtl/template"
+	"net/http"
+	"text/template"
+)
+
+type Hangman struct {
+}
+
+func main() {
+
+	fs := http.FileServer(http.Dir("css"))
+	http.Handle("/css/style", http.StripPrefix("/css/", fs))
+
+	tmpl := template.Must(template.ParseFiles("index.html"))
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		data := Hangman
+
+		tmpl.Execute(w, data)
+	})
+	http.ListenAndServe(":80", nil)
+
+}
